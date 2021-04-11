8a50d3d5949f7ac706876a789b5e140857539389
GhzGarage <qbcore@programmer.net>
2021-04-10 21:53:09 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-HouseRobbery'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua',
+    'config.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+    'config.lua',
+}
