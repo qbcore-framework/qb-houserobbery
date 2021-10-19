34b056b601829f7d8298692457f8fbc1d8d1ef6b
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-10-19 16:04:56 +0200

    feat(client): Change invokenative to actual function

--- client/main.lua
+++ client/main.lua
@@ -243,7 +243,7 @@ function PoliceCall()
     if math.random(1, 100) <= chance then
         local closestPed = GetNearbyPed()
         if closestPed ~= nil then
-            local s1, s2 = Citizen.InvokeNative(0x2EB41072B4C1E4C0, pos.x, pos.y, pos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt())
+            local s1, s2 = GetStreetNameAtCoord(pos.x, pos.y, pos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt())
             local streetLabel = GetStreetNameFromHashKey(s1)
             local street2 = GetStreetNameFromHashKey(s2)
             if street2 ~= nil and street2 ~= "" then 
