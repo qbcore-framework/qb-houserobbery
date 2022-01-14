1a6c900252c0c594f34be1c3bc43abe2bcdd1844
ayatollah <tobias.bovang@supergott.com>
2022-01-14 14:48:31 +0100

    Create sv.lua
    Added swedish locale

---
+++ locales/sv.lua
@@ -0,0 +1,16 @@
+local Translations = {
+    error = {
+        ["missing_something"] = "Det verkar som att du saknar något...",
+        ["not_enough_police"] = "Inte tillräckligt med poliser..",
+        ["door_open"] = "Dörren är redan öppen..",
+        ["process_cancelled"] = "Process avbruten..",
+        ["didnt_work"] = "Det fungerade inte..",
+        ["emty_box"] = "Lådan är tom..",
+    },
+    success = {
+        ["worked"] = "Det fungerade!",
+    }
+}
+Lang = Locale:new({
+phrases = Translations,
+warnOnMissing = true})
